fe81a66df4ce93c07906437176fddc13942cf277
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-12-03 11:28:09 +0100

    day 3 part 2

--- src/day03/Main.kt
+++ src/day03/Main.kt
@@ -3,20 +3,47 @@ package day03
 import java.io.File
 
 fun main() {
-    val text = File("src/day03/input.txt").readText()
+    val text = File("src/day03/input.txt").readText().filterNot { it.isWhitespace() }
     println(part1(text))
+    println(part2(text))
 }
 
 fun part1(text: String): Int {
     val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
 
-    val matches = regex.findAll(text.filterNot { it.isWhitespace() })
-    val multiplications = matches.map { mul ->
-        mul.value
-            .split("mul(", ",", ")").filterNot { it == "mul(" || it == "," || it == ")" || it.isEmpty() }
+    val matches = regex.findAll(text).map { it.value }.toList()
+    return calculateMuls(matches)
+}
+
+fun part2(text: String): Int {
+    val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)")
+
+    val matches = regex.findAll(text).map { it.value }.toList()
+
+    val stringMuls = mutableListOf<String>()
+
+    var enabled = true
+    for (match in matches) {
+        if (match == "do()") {
+            enabled = true
+        } else if (match == "don't()") {
+            enabled = false
+        } else if (enabled) {
+            stringMuls.add(match)
+        }
+    }
+
+    return calculateMuls(stringMuls)
+}
+
+
+fun calculateMuls(stringMuls: List<String>): Int {
+    val multiplications = stringMuls.map { mul ->
+        mul
+            .split("mul(", ",", ")")
+            .filterNot { it == "mul(" || it == "," || it == ")" || it.isEmpty() }
             .map { it.toInt() }
     }.toList()
 
     return multiplications.sumOf { it.reduce { acc, int -> acc * int } }
 }
-
